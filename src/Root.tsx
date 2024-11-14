6600e15797edb741110244ebcde334b0f47f01c6
Denys Lysiak <lisyak99.dl@gmail.com>
2024-11-14 15:41:31 +0200

    trying to solve error

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,23 +1,23 @@
-import React from "react"
-import { Provider } from "react-redux"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { store } from "./app/store"
-import App from "./App"
-import { ProductListView } from "./components/ProductListView/ProductListView"
-import { ProductView } from "./components/ProductView/ProductView"
+import React from "react";
+import { Provider } from "react-redux";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { store } from "./app/store";
+import App from "./App";
+import { ProductListView } from "./components/ProductListView/ProductListView";
+import { ProductView } from "./components/ProductView/ProductView";
 
 export const Root: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Provider store={store} >
+    <HashRouter>
+      <Provider store={store}>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<ProductListView />} />
-            
+
             <Route path="productView" element={<ProductView />} />
           </Route>
         </Routes>
       </Provider>
-    </BrowserRouter>
-  )
-}
+    </HashRouter>
+  );
+};
